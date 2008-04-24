31fd60bfe2b27c8a7abd658013925b4dab36a617
bruno.ethvignot <bruno.ethvignot@014c92e9-d94a-0410-9540-2704d1e34f37>
2008-04-24 20:10:38 +0000

    first commit script don't work!

---
+++ conf.pl
@@ -0,0 +1,415 @@
+
+{[
+	
+{	titre => 'Linux (fr)',
+	page => 'linux.html',
+	sites => [
+	
+	{	titre => 'LinuxFR.org',
+		url => 'http://www.linuxfr.org/',
+		rss => 'http://linuxfr.org/backend/news/rss20.rss',
+		lang => 'fr'
+	},
+	{	titre => 'Toolinux',
+		url => 'http://www.toolinux.com/',
+		rss => 'http://www.toolinux.com/backend.php3',
+		lang => 'fr'
+	},
+	{	titre => 'LinuxFr.org - Journaux',
+		url => 'http://linuxfr.org/journal/',
+		rss => 'http://linuxfr.org/backend/journaux/rss20.rss',
+		lang => 'fr'
+	},
+#	{	titre => 'Labo-Linux - News',
+#		url => 'http://www.labo-linux.org',
+#		rss => 'http://www.labo-linux.org/rss/news.php?type=news&amp;nombre=30',
+#		lang => 'fr',
+#		encoding => 'iso-8859-1'
+#	},
+	{	titre => 'Lolix',
+		url => 'http://www.lolix.org/',
+		rss => 'http://fr.lolix.org/jobs.rss',
+		lang => 'fr'
+	},
+	{	titre => 'GCU Squad',
+		url => 'http://www.gcu-squad.org/',
+		rss => 'http://www.gcu.info/feed/',
+		lang => 'fr',
+		encoding => 'utf-8'
+		#encoding => 'iso-8859-1'
+	},
+	{	titre => 'Linux French',
+		url => 'http://www.linuxfrench.net/',
+		rss => 'http://www.linuxfrench.net/backend.php3',
+		lang => 'fr'
+	},
+	{	titre => 'Lea',
+		url => 'http://www.lea-linux.org/',
+		rss => 'http://www.lea-linux.org/_src/rss.php?max=10',
+		lang => 'fr'
+	},
+	{	titre => 'Linux Graphic',
+		url => 'http://www.linuxgraphic.org/',
+		rss => 'http://www.linuxgraphic.org/backend_actualites.php',
+		lang => 'fr'
+	},
+	{	titre => 'UnixTech',
+		url => 'http://unixtech.be/',
+		rss => 'http://www.unixtech.be/backend.php',
+		lang => 'fr'
+	}
+	]
+},
+
+{	titre => 'Linux (en)',
+	page => 'linux-en.html',
+
+	sites => [
+	{	titre => 'Slashdot',
+		url => 'http://www.slashdot.org/',
+		rss => 'http://rss.slashdot.org/Slashdot/slashdot',
+		lang => 'en'
+	},
+	{	titre => 'Linux Today',
+		url => 'http://www.linuxtoday.com/',
+		rss => 'http://linuxtoday.com/backend/biglt.rss',
+		lang => 'en'
+	},
+	{	titre => 'Linux Weekly News',
+		url => 'http://lwn.net/',
+		rss => 'http://lwn.net/headlines/newrss',
+		lang => 'en'
+	},
+	{	titre => 'Linux Games',
+		url => 'http://www.linuxgames.com/',
+		rss => 'http://www.linuxgames.com/news/linuxgames.rss',
+		lang => 'en'
+	},
+	{	titre => 'OSNews',
+		url => 'http://www.osnews.com/',
+		rss => 'http://www.osnews.com/files/recent.rdf',
+		lang => 'en'
+	},
+	{	titre => 'Linux.com',
+		url => 'http://www.linux.com/feature/',
+		rss => 'http://www.linux.com/feature/?theme=rss',
+		lang => 'en'
+	},
+#	{	titre => 'NewsForge',
+#		url => 'http://www.newsforge.com/',
+#		rss => 'http://www.newsforge.com/index.rss',
+#		lang => 'en'
+#	},
+	{	titre => 'Advogato',
+		url => 'http://www.advogato.org/',
+		rss => 'http://www.advogato.org/rss/articles.xml',
+		lang => 'en'
+	},
+	{	titre => 'Root Prompt',
+		url => 'http://rootprompt.org/',
+		rss => 'http://rootprompt.org/rss/',
+		lang => 'en'
+	}
+	]
+},
+
+{	
+	titre => 'S&eacute;curit&eacute;',
+	page => 'securite.html',
+	sites => [
+	{	titre => 'Br&egrave;ves de s&eacute;curit&eacute;s HSC',
+		url => 'http://www.hsc.fr/ressources/breves/',
+		rss => 'http://www.hsc.fr/ressources/breves/breves.rss',
+		lang => 'en'
+	},
+	{	titre => 'BugTraq',
+		url => 'http://www.securityfocus.com/archive/1',
+		rss => 'http://www.securityfocus.com/rss/vulnerabilities.xml',
+		lang => 'en'
+	},
+	{	titre => 'Securty Focus',
+		url => 'http://www.securityfocus.com/',
+		rss => 'http://www.securityfocus.com/rss/news.xml',
+		lang => 'en'
+	},
+	{	titre => 'O.S.V.D.B.',
+		url => 'http://www.osvdb.org/',
+		rss => 'http://www.osvdb.org/backend/rss.php?n=10',
+		lang => 'en'
+	},
+	{	titre => '2600',
+		url => 'http://www.2600.com/',
+		rss => 'http://www.2600.com/rss.xml',
+		lang => 'en'
+	},
+	{	titre => 'Nouveaut&eacute;s HSC',
+		url => 'http://www.hsc.fr/',
+		rss => 'http://www.hsc.fr/nouveautes.en.xml',
+		lang => 'en'
+	},
+	{	titre => 'Prelude IDS',
+		url => 'http://www.prelude-ids.org/',
+		rss => 'http://www.prelude-ids.org/backend.php3',
+		lang => 'en'
+	},
+	{	titre => 'Vulnerabilite.com',
+		url => 'http://www.vulnerabilite.com/',
+		rss => 'http://www.vulnerabilite.com/rss/actualite.xml',
+		lang => 'fr'
+	},
+	{	titre => 'Cisco Security',
+		url => 'http://newsroom.cisco.com/dlls/index.html',
+		rss => 'http://newsroom.cisco.com/data/syndication/rss2/newsReleases_20.xml',
+		lang => 'en'
+	},
+	{	titre => 'Linux Security',
+		url => 'http://www.linuxsecurity.com/',
+		rss => 'http://www.linuxsecurity.com/static-content/linuxsecurity_advisories.rdf',
+		lang => 'en'
+	}
+  ]
+},
+
+{	titre => 'Projets',
+	page => 'projets.html',
+	sites => [
+	{	titre => 'GNOME News',
+		url => 'http://www.gnomedesktop.org/',
+		rss => 'http://gnomedesktop.org/node/feed',
+		lang => 'en'
+	},
+	{	titre => 'KDE News',
+		url => 'http://dot.kde.org/',
+		rss => 'http://www.kde.org/dotkdeorg.rdf',
+		lang => 'en'
+	},	
+	{	titre => 'Mono',
+		url => 'http://www.mono-project.com/Main_Page',
+		rss => 'http://www.mono-project.com/news/index.rss2',
+		lang => 'en'
+	},	
+	{	titre => 'Zope.org',
+		url => 'http://www.zope.org/',
+		rss => 'http://www.zope.org/news.rss',
+		lang => 'en'
+	},	
+
+	{	titre => 'Planet Jabber',
+		url => 'http://www.planetplanet.org/',
+		rss => 'http://planet.jabber.org/atom.xml',
+		lang => 'en'
+	},	
+
+	{	titre => 'Dell Linux',
+		url => 'http://linux.dell.com/',
+		rss => 'http://linux.dell.com/blog/index.rss',
+		lang => 'en'
+	},	
+	{	titre => 'Apache France',
+		url => 'http://www.apachefrance.com/',
+		rss => 'http://www.apachefrance.com/News/Rss/news-apachefrance.rss',
+		lang => 'fr'
+	}	
+	]
+},
+
+{	titre=> 'BSD',
+	page => 'bsd.html',
+	sites => [
+	{	titre => 'GCU Squad',
+		url => 'http://www.gcu-squad.org/',
+		rss => 'http://www.gcu.info/feed/',
+		lang => 'fr',
+		encoding => 'iso-8859-1'
+	},
+	{	titre => 'The Daily DaemonNews',
+		url => 'http://bsdnews.com/',
+		rss => 'http://bsdnews.com/ddn.rdf.php3',
+		lang => 'en'
+	},
+	{	titre => 'OpenBSD Journal',
+		url => 'http://www.undeadly.org/',
+		rss => '"http://undeadly.org/cgi?action=rss"',
+		lang => 'en'
+	},
+	{	titre => 'The FreeBSD Diary',
+		url => 'http://www.freebsddiary.org/',
+		rss => 'http://www.freebsddiary.org/news.php',
+		lang => 'en'
+	},
+	{	titre => 'NetBSD News',
+		url => 'http://www.netbsd.org/',
+		rss => 'http://www.netbsd.org/Changes/rss-netbsd.xml',
+		lang => 'en'
+	},
+	{	titre => 'NetBSD Code Changes',
+		url => 'http://www.netbsd.org/',
+		rss => 'http://www.netbsd.org/Changes/rss-netbsd-internals.xml',
+		lang => 'en'
+	},
+	{	titre => 'DragonFly BSD Digest',
+		url => 'http://www.shiningsilence.com/dbsdlog',
+		rss => 'http://www.shiningsilence.com/dbsdlog/index.php/feed',
+		lang => 'en'
+	},
+   
+	]
+},
+{	titre => 'D&eacute;veloppement',
+	page => 'dev.html',
+	sites => [
+	{	titre => 'Use Perl',
+		url => 'http://use.perl.org/',
+		rss => 'http://use.perl.org/index.rss',
+		lang => 'en'
+	},
+	{	titre => 'Application Servers',
+		url => 'http://www.application-servers.com/',
+		rss => 'http://www.application-servers.com/rss/appservers.rss',
+		lang => 'fr'
+	},
+	{	titre => 'Java Channel',
+		url => 'http://www.java-channel.org/',
+		rss => 'http://www.java-channel.org/exportRSS-091.jsp',
+		lang => 'en'
+	},
+	{	titre => 'XML fr',
+		url => 'http://xmlfr.org/',
+		rss => 'http://xmlfr.org/actualites/general.rss10',
+		lang => 'fr'
+	},
+	{	titre => 'Dev Shed',
+		url => 'http://www.devshed.com/',
+		rss => 'http://www.devshed.com/index2.php?option=mos_rss&no_html=1',
+		lang => 'en'
+	},
+	{	titre => 'Kernel Traffic',
+		url => 'http://www.kerneltraffic.org/',
+		rss => 'http://www.kerneltraffic.org/kernel-traffic/rss.rdf',
+		lang => 'en'
+	},
+	{	titre => 'ONJava',
+		url => 'http://www.onjava.com/',
+		rss => 'http://www.oreillynet.com/meerkat/?_fl=rss10&t=ALL&c=5136',
+		lang => 'en'
+	},
+	{	titre => 'Latest RFCs',
+		url => 'http://rfc.x42.com/',
+		rss => 'http://x42.com/rss/rfc.rss',
+		lang => 'en'
+	},
+	{	titre => 'PHPBuilder',
+		url => 'http://www.phpbuilder.com/',
+		rss => 'http://www.phpbuilder.com/rss_feed.php?type=articles&limit=20',
+		lang => 'en'
+	}
+	]
+},
+{	titre => 'Applications',
+	page => 'applications.html',
+	sites => [
+	{	titre => 'Je suis libre',
+		url => 'http://www.jesuislibre.org/',
+		rss => '',
+		lang => 'fr'
+	},
+	{	titre => 'Freshmeat',
+		url => 'http://freshmeat.net/',
+		rss => 'http://images.feedstermedia.com/feedcache/ostg/freshmeat/fm-releases-global.xml',
+		lang => 'en'
+	},
+	{	titre => 'Linux Games',
+		url => 'http://www.linuxgames.com/',
+		rss => 'http://www.linuxgames.com/news/linuxgames.rss',
+		lang => 'en'
+	},
+	{	titre => 'IceWalkers',
+		url => 'http://www.icewalkers.com/',
+		rss => 'http://www.icewalkers.com/backend/icewalkers.rss',
+		lang => 'en'
+	}
+	]
+},
+{	titre => 'Mozilla',
+	page => 'mozilla.html',
+	sites => [
+	{	titre => 'MozillaZine-fr',
+		url => 'http://www.mozillazine-fr.org/',
+		rss => 'http://www.mozillazine-fr.org/contenu.xml',
+		lang => 'fr'
+	},	
+	{	titre => 'MozillaZine',
+		url => 'http://www.mozillazine.org/',
+		rss => 'http://www.mozillazine.org/contents.rdf',
+		lang => 'en'
+	},	
+	{	titre => 'MozillaZine-fr Revue',
+		url => 'http://www.mozillazine-fr.org/blog/',
+		rss => 'http://www.mozillazine-fr.org/blog/rss.php',
+		lang => 'fr',
+		encoding => 'utf-8'
+	},	
+	{	titre => 'Standblog',
+		url => 'http://standblog.org/',
+		#rss => 'http://standblog.org/blog/feed/rss2',
+    rss => 'http://standblog.org/blog/feed/atom',
+		lang => 'fr',
+		#encoding => 'utf-8'
+	},	
+	{	titre => 'Be:Fox',
+		url => 'http://www.befox.be/', 
+		rss => 'http://feeds.feedburner.com/Befox',
+		lang => 'fr'
+	},
+	{	titre => 'blog.mozbox.org',
+		url => 'http://blog.mozbox.org/', 
+		rss => 'http://blog.mozbox.org/feed/rss2',
+		lang => 'fr'
+	},
+	{	titre => 'jy[B]log',
+		url => 'http://ljouanneau.com/blog/', 
+		rss => 'http://ljouanneau.com/dotclear/rss.php',
+		lang => 'fr'
+	},
+	{	titre => 'Carnet Web de Pascal',
+		url => 'http://www.chevrel.org/fr/carnet/', 
+		rss => 'http://www.chevrel.org/fr/carnet/rss.php',
+		lang => 'fr'
+	}
+
+	]
+},
+{	titre => 'Autres',
+	page => 'autres.html',
+	sites => [
+	{	titre => 'MacBidouille',
+		url => 'http://www.macbidouille.com/',
+		rss => 'http://www.macbidouille.com/macbidouille.rss',
+		lang => 'fr'
+	},
+	{	titre => 'Wlanfr',
+		url => 'http://www.wlanfr.net/',
+		rss => 'http://www.wlanfr.net/backend.rss',
+		lang => 'fr'
+	},
+	{	titre => 'La Paix Maintenant',
+		url => 'http://www.lapaixmaintenant.org/',
+		rss => '',
+		lang => 'fr'
+	},
+	{	titre => 'Kuro5hin',
+		url => 'http://www.kuro5hin.org/i',
+		rss => 'http://www.kuro5hin.org/backend.rdf',
+		lang => 'en'
+	},
+	{	titre => 'FSF France',
+		url => 'http://fsffrance.org/',
+		rss => 'http://fsffrance.org/news/fsfe-fr-channel.fr.xml',
+		lang => 'fr'
+	}
+	]
+}
+
+]};
+
+
